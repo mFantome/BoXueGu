a4e0ebc7a5f8435b75525208a964546d9a721928
mFantome <2226300934@qq.com>
2017-12-25 15:34:00 +0800

    1

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
@@ -1,8 +1,11 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import cn.edu.gdmec.android.boxuegu.R;
 
@@ -15,5 +18,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        init();
+    }
+
+    private void init() {
+        TextView tv_version = (TextView) findViewById(R.id.tv_version);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
+            tv_version.setText("V"+info.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
     }
 }

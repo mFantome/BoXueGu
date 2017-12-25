6b25af0c36f55d1911ed49bc7fcd6bfa465dc436
mFantome <2226300934@qq.com>
2017-12-25 15:19:23 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package cn.edu.gdmec.android.boxuegu.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.edu.gdmec.android.boxuegu.R;
+
+/**
+ * Created by student on 17/12/25.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

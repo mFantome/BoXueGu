e34b03d6cc815111bad348825f6ad4f4fb771e73
z997097287 <997097287@qq.com>
2017-12-26 16:24:50 +0800

    第6章做了一半（习题节目逻辑代码差一点，今晚完成第六章）

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/MainActivity.java
@@ -2,15 +2,18 @@ package cn.edu.gdmec.android.boxuegu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     private void init(){
-        
+        tv_back = ((TextView) findViewById(R.id.tv_back));
+        TextView tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("博学谷课程");
     }
 }
---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/adapter/ExercisesAdapter.java
@@ -0,0 +1,99 @@
+package cn.edu.gdmec.android.boxuegu.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.edu.gdmec.android.boxuegu.R;
+import cn.edu.gdmec.android.boxuegu.bean.ExercisesBean;
+
+/**
+ * Created by student on 17/12/26.
+ */
+
+public class ExercisesAdapter extends BaseAdapter{
+    private Context mContext;
+
+    public ExercisesAdapter(Context mContext){
+        this.mContext = mContext;
+    }
+
+    private List<ExercisesBean> eb1;
+
+    /**
+     * 设置数据并更新页面
+     * @param eb1
+     */
+    public void setData(List<ExercisesBean> eb1){
+        this.eb1 = eb1;
+        notifyDataSetChanged();
+    }
+
+    /**
+     * 获取item的总数
+     * @return
+     */
+    @Override
+    public int getCount() {
+        return eb1 == null ? 0 : eb1.size();
+    }
+
+    /**
+     * 根据position得到对应的Item对象
+     * @param position
+     * @return
+     */
+    @Override
+    public Object getItem(int position) {
+        return eb1 == null ? null : eb1.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View converView, ViewGroup parent) {
+        final ViewHolder vh;
+        //复用converView
+        if(converView == null){
+            vh = new ViewHolder();
+            converView = LayoutInflater.from(mContext).inflate(R.layout.exercises_list_item,null);
+            vh.title = converView.findViewById(R.id.tv_title);
+            vh.content = converView.findViewById(R.id.tv_content);
+            vh.order = converView.findViewById(R.id.tv_order);
+            converView.setTag(vh);
+        }else {
+            vh = (ViewHolder) converView.getTag();
+        }
+        //获取position对应的item对象
+        final ExercisesBean bean = (ExercisesBean) getItem(position);
+        if(bean != null){
+            vh.order.setText(position + 1 + "");
+            vh.title.setText(bean.title);
+            vh.content.setText(bean.content);
+            vh.order.setBackgroundResource(bean.background);
+
+        }
+        converView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bean == null){
+                    return;
+                }
+                //跳转到习题详情页
+            }
+        });
+        return converView;
+    }
+    class ViewHolder{
+        public TextView title,content;
+        public TextView order;
+    }
+}

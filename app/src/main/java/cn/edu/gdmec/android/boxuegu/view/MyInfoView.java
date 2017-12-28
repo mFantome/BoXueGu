361ef96df9086e6de7f20bcaf03af463dca9eace
zhengyuting17 <282966276@qq.com>
2017-12-28 11:33:32 +0800

    改动在MainActivity,此处有bug

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/view/MyInfoView.java
@@ -0,0 +1,18 @@
+package cn.edu.gdmec.android.boxuegu.view;
+
+import android.view.View;
+
+import cn.edu.gdmec.android.boxuegu.MainActivity;
+
+/**
+ * Created by acer on 2017/12/26.
+ */
+
+public class MyInfoView {
+    public MyInfoView(MainActivity mainActivity) {
+    }
+
+    public View getView() {
+        return null;
+    }
+}
